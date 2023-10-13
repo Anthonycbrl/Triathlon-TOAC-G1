23f7e27938bcae4c3b21dd13f4c134991e74b398
quentin-beaufort <118752155+quentin-beaufort@users.noreply.github.com>
2023-10-13 17:56:10 +0200

    Mise à jour de la semaine du code wordpress et de la BD du serveur AWS

--- code/dev-aws/wordpress/wp-config-sample.php
+++ code/dev-aws/wordpress/wp-config-sample.php
@@ -21,13 +21,13 @@
 
 // ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
 /** Nom de la base de données de WordPress. */
-define('DB_NAME', 'wp_db');
+define('DB_NAME', 'votre_nom_de_bdd');
 
 /** Utilisateur de la base de données MySQL. */
-define('DB_USER', 'ec2-user');
+define('DB_USER', 'votre_utilisateur_de_bdd');
 
 /** Mot de passe de la base de données MySQL. */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'votre_mdp_de_bdd');
 
 /** Adresse de l’hébergement MySQL. */
 define('DB_HOST', 'localhost');
